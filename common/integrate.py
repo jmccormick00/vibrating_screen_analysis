146aaf226c78d63bd35b94e9634ed3a8fda82dc4
jmccormick00 <jmccormick00@gmail.com>
2015-07-31 15:22:13 -0400

    Changed file structure around to make things easier to work with

--- common/integrate.py
+++ common/integrate.py
@@ -1,7 +1,6 @@
 __author__ = 'jrmccormick'
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def integrate(accel, T):
@@ -46,53 +45,3 @@ def integrate(accel, T):
     # Fourier transform and return the displacement, frequencies, and amplitudes
     x = np.fft.ifft(fft_x)
     return x, freq_fold, amplitude_fold
-
-
-def read_datafile(filename):
-    X = []
-    Y = []
-    Z = []
-
-    datafile = open(filename)
-    for line in datafile:
-        x, y, z = line.split()
-        X.append(float(x))
-        Y.append(float(y))
-        Z.append(float(z))
-    datafile.close()
-
-    return np.array(X), np.array(Y), np.array(Z)
-
-
-def main():
-    fs = 1500.0  # Sampling Frequency (Hz)
-    Ts = 1/fs  # Sample time, period
-
-    # Compute the time vector
-    t = np.arange(0.0, 0.2, Ts, dtype=float)
-
-    calc_x, freq, ffta = integrate(a, Ts)
-
-    plt.subplot(3,1,1)
-    plt.plot(t, a)
-    plt.grid()
-    plt.xlabel("time (t)")
-    plt.ylabel("Accel (m/s/s)")
-    plt.subplot(3,1,2)
-    plt.plot(t, calc_x, '--r')
-    plt.legend()
-    plt.grid()
-    plt.xlabel("time (t)")
-    plt.ylabel("X (m)")
-    plt.subplot(3,1,3)
-    plt.plot(freq, np.abs(ffta), 'r')
-    plt.grid()
-    plt.xlabel("Frequency (Hz)")
-    plt.ylabel("Amplitude")
-
-    plt.show()
-
-
-# call main
-if __name__ == '__main__':
-    main()
