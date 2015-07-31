8d123aa213f03d758874f1fe4e0bf884ae3cb724
jmccormick00 <jmccormick00@gmail.com>
2015-07-31 16:54:18 -0400

    Updated the comments

--- motion_capturing/integrate.py
+++ motion_capturing/integrate.py
@@ -3,24 +3,25 @@ __author__ = 'jrmccormick'
 import numpy as np
 
 
-def integrate(accel, T):
-    # The integration method
-    # Input:
-    #       accel - the acceleration vector
-    #       T - the period, the sampling time (t1 + T = t2)
-    # Takes an array of acceleration values recorded at equally spaced time intervals
-    # and performs a double integral on them to get displacement.
-    # It performs the integration by converting the time(t) vs acceleration data from
-    # time domain to frequency domain by utilizing a Fourier transform
-    # In frequency domain a simplified form of displacement has the following form:
-    # x = A*sin(omega*t)    Taking the derivative of x yields velocity and acceleration
-    # v = omega*A*cos(omega*t)
-    # a = -(omega*omega)*A*sin(omega*t)
-    # Thus in frequency domain, to perform a double integral you divide acceleration by -(omega^2)
-    # Once the division is performed, perform an inverse Fourier transform to get back time(t) vs displacement
+def integrate(acceleration, T):
+    """
+    Takes an array of acceleration values recorded at equally spaced time intervals
+    and performs a double integral on them to get displacement.
+    It performs the integration by converting the time(t) vs acceleration data from
+    time domain to frequency domain by utilizing a Fourier transform
+    In frequency domain a simplified form of displacement has the following form:
+    x = A*sin(omega*t)    Taking the derivative of x yields velocity and acceleration
+    v = omega*A*cos(omega*t)
+    a = -(omega*omega)*A*sin(omega*t)
+    Thus in frequency domain, to perform a double integral you divide acceleration by -(omega^2)
+    Once the division is performed, perform an inverse Fourier transform to get back time(t) vs displacement
+    :param accel: acceleration vector
+    :param T: T - the period, the sampling time (t1 + T = t2)
+    :return: the displacement vector, frequency vector, amplitude vector
+    """
 
     # First - Take the Fourier transform
-    fft_a = np.fft.fft(accel)
+    fft_a = np.fft.fft(acceleration)
 
     # Second - Calculate the frequencies based on the number of points and the period
     n = len(fft_a)
@@ -29,7 +30,7 @@ def integrate(accel, T):
 
     # Third - Note that the frequencies in the FFT and the freq vector go from zero to some
     # large positive number then from a large negative number back toward zero. It is common
-    # to look at just the first half of the unshifted FFT and frequency vectors and fold all the
+    # to look at just the first half of the un-shifted FFT and frequency vectors and fold all the
     # amplitude info into the positive frequencies.  Furthermore, to get amplitude right, you must
     # normalize by the length of the original FFT.  Note the (2/n) in the following accomplishes
     # both the folding and scaling.
