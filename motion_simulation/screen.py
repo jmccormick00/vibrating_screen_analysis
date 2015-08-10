c5e7ef2a59ad1409dd88a37f3de15a30a4ae13e3
jmccormick00 <jmccormick00@gmail.com>
2015-08-10 16:03:48 -0400

    Removed the positional calculations.

--- motion_simulation/screen.py
+++ motion_simulation/screen.py
@@ -3,7 +3,7 @@ __author__ = 'jrmccormick'
 import numpy as np
 
 
-class counterweight:
+class CounterWeight:
     def __init__(self, posX, posY, radius, weight, rpm, startAngle, counterclockwise):
         '''
         :param posX: The x position in relation to the center of gravity (ft)
@@ -26,7 +26,6 @@ class counterweight:
         if not counterclockwise:
             self.omega=-self.omega
 
-
     def calculate_forces(self, t):
         '''
         :param t: A single time or an array of the time instances to use to solve the forces
@@ -44,36 +43,24 @@ class counterweight:
         return self.mass*self.omega*self.omega*self.radius*np.sin(self.startAngle+self.omega*t)-self.weight
 
 
-class screen:
-    def __init__(self, weight, momentInertia, alpha, feedSpringPos, dischargeSpringPos, feedSpringStiff, dischargeSpringStiff):
-        # Adjusted Spring Stiffness based on screen body angle
-        self.Sfax=feedSpringStiff[1]*np.sin(alpha)+feedSpringStiff[0]*np.cos(alpha)
-        self.Sfay=feedSpringStiff[1]*np.cos(alpha)+feedSpringStiff[0]*np.sin(alpha)
-        self.Sdax=dischargeSpringStiff[1]*np.sin(alpha)+dischargeSpringStiff[0]*np.cos(alpha)
-        self.Sday=dischargeSpringStiff[1]*np.cos(alpha)+dischargeSpringStiff[0]*np.sin(alpha)
+class Screen:
+    def __init__(self, weight, momentInertia):
 
         self.mass=weight/32.17405 # slugs
         self.weight=weight
-        self.alpha=np.deg2rad(alpha) # Body angle, radians
+        #self.alpha=np.deg2rad(alpha) # Body angle, radians
         self.momentInertia=momentInertia
-        self.feedSpringPos=feedSpringPos
-        self.dischargeSpringPos=dischargeSpringPos
-        self.feedSpringStiff=feedSpringStiff
-        self.dischargeSpringStiff=dischargeSpringStiff
+        #self.feedSpringPos=feedSpringPos
+        #self.dischargeSpringPos=dischargeSpringPos
+        #self.feedSpringStiff=feedSpringStiff
+        #self.dischargeSpringStiff=dischargeSpringStiff
 
         self.cwArr=[]
 
     def addCounterWeight(self, cw):
         self.cwArr.append(cw)
 
-    def calculate_acceleration(self, state, t):
-        X0 = state[0]
-        dX0 = state[1]
-        Y0 = state[2]
-        dY0 = state[3]
-        #Theta0 = state[4]
-        #dTheta0 = state[5]
-
+    def calculate_acceleration(self, t):
         Theta0=0.0
         rx=ry=0.0
         mrx=mry=0.0
@@ -84,13 +71,9 @@ class screen:
             mrx += cw.fx*cw.posY
             mry += cw.fy*cw.posX
 
-        Dfx=X0+(self.feedSpringPos[0]*np.cos(Theta0)-self.feedSpringPos[1]*np.sin(Theta0))-self.feedSpringPos[0]
-        Dfy=Y0+(self.feedSpringPos[0]*np.sin(Theta0)+self.feedSpringPos[1]*np.cos(Theta0))-self.feedSpringPos[1]
-        Ddx=X0+(self.dischargeSpringPos[0]*np.cos(Theta0)-self.dischargeSpringPos[1]*np.sin(Theta0))-self.dischargeSpringPos[0]
-        Ddy=Y0+(self.dischargeSpringPos[0]*np.sin(Theta0)+self.dischargeSpringPos[1]*np.cos(Theta0))-self.dischargeSpringPos[1]
 
-        ddX0=(rx-(2*self.Sfax*Dfx)-(2*self.Sdax*Ddx))/self.mass
-        ddY0=(ry-(2*self.Sfay*Dfy)-(2*self.Sday*Ddy))/self.mass
+        ddX0 = (rx-(2*self.Sfax*Dfx)-(2*self.Sdax*Ddx))/self.mass
+        ddY0 = (ry-(2*self.Sfay*Dfy)-(2*self.Sday*Ddy))/self.mass
         #ddTheta0=(mry+mrx+(-self.dischargeSpringPos[1]*2*self.Sdax*Ddx)+(self.dischargeSpringPos[0]*2*self.Sday*Ddy)-(self.feedSpringPos[1]*2*self.Sfax*Dfx)+(self.feedSpringPos[0]*2*self.Sfay*Dfy))/self.momentInertia
 
-        return [dX0, ddX0, dY0, ddY0] #, dTheta0, ddTheta0]
+        return ddX0, ddY0
